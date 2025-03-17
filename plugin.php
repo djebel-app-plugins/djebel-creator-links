6b50eb7c8a8a8faf8cb9c21deb36a9ee6fd170d1
Slavi Marinov <admin@orbisius.com>
2025-03-17 23:22:44 +0200

    a

--- plugin.php
+++ plugin.php
@@ -1,10 +1,9 @@
 <?php
 /*
-plugin_name: Djebel Social Pages
-plugin_uri: https://djebel.com/plugins/social-pages
+plugin_name: Djebel Creator Links
+plugin_uri: https://djebel.com/plugins/create-links
 description: allows content creators to create social links to their social profiles.
 version: 1.0.0
-load_priority:20
 tags: socia,social media,pages
 stable_version: 1.0.0
 min_php_ver: 5.6
