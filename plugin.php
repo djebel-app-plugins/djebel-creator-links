2ad8e2c203dc79fc6508abb7f1c83c60dab031c0
Slavi Marinov <admin@orbisius.com>
2025-03-17 22:53:20 +0200

    initial add

---
+++ plugin.php
@@ -0,0 +1,25 @@
+<?php
+/*
+plugin_name: Djebel Social Pages
+plugin_uri: https://djebel.com/plugins/social-pages
+description: allows content creators to create social links to their social profiles.
+version: 1.0.0
+load_priority:20
+tags: socia,social media,pages
+stable_version: 1.0.0
+min_php_ver: 5.6
+min_dj_cms_ver: 1.0.0
+tested_with_dj_cms_ver: 1.0.0
+author_name: Svetoslav Marinov (Slavi)
+company_name: Orbisius
+author_uri: https://orbisius.com
+text_domain: hello-world
+license: gpl2
+*/
+
+Dj_CMS_Hooks::addAction( 'app.page.body.start', 'dj_app_social_pages_content' );
+
+function dj_app_social_pages_content()
+{
+//    echo 'Hello World' . __METHOD__;
+}
